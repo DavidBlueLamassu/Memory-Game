addba6883154353b2697df22331ec1c9c01a4361
DavidBlueLamassu <dra21@cam.ac.uk>
2023-04-17 14:04:43 +0100

    Tailwind Router setup files

---
+++ src/components/pages/Blog.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Blog() {
+  return (
+    <div>
+      <h1 className="bg-green-500">Blog Page</h1>
+    </div>
+  );
+}
+
+export default Blog;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact(props) {
+  return (
+    <div className="bg-orange-500">
+      <h1>Contact Page</h1>
+    </div>
+  );
+}
+
+export default Contact;
