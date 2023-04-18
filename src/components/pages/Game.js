9a5bbb646f099179bed1dc9c764ba59b819477bc
DavidBlueLamassu <dra21@cam.ac.uk>
2023-04-18 19:51:26 +0100

    Cards now generated in Game.js

--- src/components/pages/Game.js
+++ src/components/pages/Game.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import GemCard from "./GemCard.js";
+import gems from "./../../gems.json";
 
 let score = 0;
 let topScore = 0;
@@ -16,14 +18,13 @@ function Game() {
         <h1 className="py-10 text-6xl text-center font-bold">Clicky Game!</h1>
         <h2 className="text-center text-2xl font-bold">Click on an image to earn points, but don't click on any more than once!</h2>
       </article>
-      <main className="w-4">
-        <div className="my-80">
-        </div>
+      <main>
+        {GemDisplay()}
       </main>
       <footer >
         <div className="py-8 bg-purple-500"></div>
         <div className="flex justify-between bg-purple-800 py-2 text-white">
-        <p className="px-2 font-bold">Clicky Game!<img src="../assets/logo192.png" alt="React Spinner" className="w-5 h-5 float-right ml-2 mt-1"></img></p>
+        <p className="px-2 font-bold">Clicky Game!<img src="../assets/images/logo192.png" alt="React Spinner" className="w-5 h-5 float-right ml-2 mt-1"></img></p>
         <Link to="/" className="px-2 font-bold hover:text-red-500">
           Home Page
         </Link>
@@ -31,6 +32,26 @@ function Game() {
       </footer>
     </div>
   );
+}
+
+function GemDisplay() {
+  return( <div className="flex justify-evenly" onClick={() => {
+    identityTest();}}>
+      {
+        gems.map(function(currentObject) {
+          return <GemCard
+          key={currentObject.id}
+          location={currentObject.location} 
+          title={currentObject.title}/>
+        })
+      }
+    </div>
+  )
+}
+
+function identityTest() {
+  console.log("Test");
+  console.log(this);
 }
 
 export default Game;
