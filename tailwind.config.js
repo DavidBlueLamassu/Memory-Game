27b926241c819b5b0b206f6db1a6a711e24e340a
DavidBlueLamassu <dra21@cam.ac.uk>
2023-05-06 04:31:17 +0100

    Text flashes red or green to indicate true or false answer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        flashGreen: {
+          '50%': { color: 'green' },
+        },
+        flashGreenTwo: {
+          '50%': { color: 'green' },
+        },
+        flashRed: {
+          '50%': { color: 'red' },
+        }, 
+        flashRedTwo: {
+          '50%': { color: 'red' },
+        }
+      },
+      animation: {
+        flashGreen: 'flashGreen 1s 1',
+        flashGreenTwo: 'flashGreenTwo 1s 1',
+        flashRed: 'flashRed 1s 1',
+        flashRedTwo: 'flashRedTwo 1s 1',
+      },
+      
+    },
   },
   plugins: [],
 }
