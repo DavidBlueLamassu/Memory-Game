18b65f184a74fe1a88e77fc40d454c0090cf0689
DavidBlueLamassu <dra21@cam.ac.uk>
2023-05-06 05:32:17 +0100

    Card container shakes with wrong answer

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,28 @@ module.exports = {
         }, 
         flashRedTwo: {
           '50%': { color: 'red' },
+        },
+        shake: {
+          '0%, 100%': {transform: 'translateX(0)'},
+          '10%': {transform: 'translateX(-20px)'},
+          '20%': {transform: 'translateX(20px)'},
+          '30%': {transform: 'translateX(-20px)'},
+          '40%': {transform: 'translateX(20px)'},
+          '50%': {transform: 'translateX(-20px)'},
+          '60%': {transform: 'translateX(20px)'},
+          '70%': {transform: 'translateX(-20px)'},
+          '80%': {transform: 'translateX(20px)'},
+        },
+        shakeTwo: {
+          '0%, 100%': {transform: 'translateX(0)'},
+          '10%': {transform: 'translateX(-20px)'},
+          '20%': {transform: 'translateX(20px)'},
+          '30%': {transform: 'translateX(-20px)'},
+          '40%': {transform: 'translateX(20px)'},
+          '50%': {transform: 'translateX(-20px)'},
+          '60%': {transform: 'translateX(20px)'},
+          '70%': {transform: 'translateX(-20px)'},
+          '80%': {transform: 'translateX(20px)'},
         }
       },
       animation: {
@@ -22,6 +44,8 @@ module.exports = {
         flashGreenTwo: 'flashGreenTwo 1s 1',
         flashRed: 'flashRed 1s 1',
         flashRedTwo: 'flashRedTwo 1s 1',
+        shake: 'shake 1s 1',
+        shakeTwo: 'shakeTwo 1s 1',
       },
       
     },
