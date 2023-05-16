1bf8bfc5e16cbcc361b192a72c3ee4078e425fda
DavidBlueLamassu <dra21@cam.ac.uk>
2023-05-16 18:50:48 +0100

    Home.js, GemCard.js, tailwind.config.js proofread

--- src/components/pages/GemCard.js
+++ src/components/pages/GemCard.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 //Generates a simple card to be used in conjunction with gems.json and the map method in Game.js; id is set for click events, 
-//title information for alt and location to provide an address for stored images in the public folder. className formats the 
-//image display within a slate colored frame; there is also a shadow effect. A cursor pointer, further shadow effects and a  
+//title is set for alt and location provides an address for stored images in the public folder. The image is formatted
+//to display within a slate colored frame; there is also a shadow effect. A cursor pointer, further shadow effects and a  
 //zoom animation are activated upon hover.
 function GemCard(props) {
     return ( 
